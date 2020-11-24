9ab3f518997cf9f37bb6fbaae4daf26bf66ee9d0
JordanDaigle <jordan.daigle@loginnove.ca>
2020-11-24 10:57:48 -0500

    Update CurlFactory.php
    fix

--- src/Zipkin/Reporters/Http/CurlFactory.php
+++ src/Zipkin/Reporters/Http/CurlFactory.php
@@ -58,7 +58,7 @@ final class CurlFactory implements ClientFactory
                 return $key . ': ' . $value;
             }, \array_keys($headers), $headers);
             \curl_setopt($handle, CURLOPT_HTTPHEADER, $formattedHeaders);
-            if (isset($options['timeout_ns'])) {
+            if (isset($options['timeout_ms'])) {
                 \curl_setopt($handle, CURLOPT_TIMEOUT, $options['timeout_ms']);
             } else if (isset($options['timeout'])) {
                 \curl_setopt($handle, CURLOPT_TIMEOUT, $options['timeout']);
